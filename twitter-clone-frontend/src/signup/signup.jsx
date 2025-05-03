8e1dc8ead0b9059df06fa4c1fb25c864206ec434
jetsetradioh <jetsetradioh@protonmail.com>
2025-05-03 22:41:16 +0200

    Ändrade i App.jsx och signup.jsx

--- twitter-clone-frontend/src/signup/signup.jsx
+++ twitter-clone-frontend/src/signup/signup.jsx
@@ -1,58 +1,58 @@
 import React, { useState } from 'react';
-import './signup.css'
+import './signup.css';
 
 const Signup = () => {
-    const [formData, setFormData] = useState({
-        username: '',
-        email: '',
-        password: '',
-    });
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
 
-const handleChange = (e) => {
+  const handleChange = (e) => {
     setFormData({
-        ...formData,
-        [e.target.name]: e.target.value
+      ...formData,
+      [e.target.name]: e.target.value
     });
-};
+  };
 
-const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     console.log('Registrering klar', formData);
-    //plats för lite API-anrop sen.
-}
-}
+    // plats för API-anrop sen
+  };
 
-return (
+  return (
     <div className="signup-container">
-        <h2>Registrera dig</h2>
-        <form onSubmit={handleSubmit}>
-            <input 
-            type="text"
-            name="username"
-            placeholder="Användarnamn"
-            value={formData.username}
-            onChange={handleChange}
-            required
-            />
-            <input
-            type="email"
-            name="email"
-            placeholder="E-post"
-            value={formData.email}
-            onChange={handleChange}
-            required
-            />
-            <input
-            type="password"
-            name="password"
-            placeholder="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-            />
-            <button type="submit">Skapa konto</button>
-        </form>
+      <h2>Registrera dig</h2>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text"
+          name="username"
+          placeholder="Användarnamn"
+          value={formData.username}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="E-post"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Lösenord"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+        <button type="submit">Skapa konto</button>
+      </form>
     </div>
-)
+  );
+};
 
 export default Signup;
