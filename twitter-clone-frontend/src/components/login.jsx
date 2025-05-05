1d37b1e109982d658bb0156d85bb2ae21b71363e
FilipLiljenberg <filipliljenberg9@.com>
2025-05-05 11:19:02 +0200

    Login uppdateringar

--- twitter-clone-frontend/src/components/login.jsx
+++ twitter-clone-frontend/src/components/login.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 const Login = () => {
   const [user, setUser] = useState({ name: "", password: "" });
+  const [toggle, setToggle] = useState(true);
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -13,23 +14,28 @@ const Login = () => {
       <h2>Logga in på Twitter</h2>
 
       <form onSubmit={submitHandler}>
-        <input
-          required
-          type="text"
-          placeholder="Username"
-          value={user.name}
-          onChange={({ target }) => setUser({ ...user, name: target.value })}
-        />
-        <input
-          required
-          type="password"
-          placeholder="Password"
-          value={user.password}
-          onChange={({ target }) =>
-            setUser({ ...user, password: target.value })
-          }
-        />
-        <input type="submit" placeholder="Login"></input>
+        {toggle ? (
+          <input
+            type="text"
+            placeholder="Username"
+            value={user.name}
+            onChange={({ target }) => setUser({ ...user, name: target.value })}
+          />
+        ) : (
+          <input
+            type="password"
+            placeholder="Password"
+            value={user.password}
+            onChange={({ target }) =>
+              setUser({ ...user, password: target.value })
+            }
+          />
+        )}
+        {toggle ? (
+          <button onClick={() => setToggle((...prev) => !prev)}>Nästa</button>
+        ) : (
+          <input type="submit" value="Login"></input>
+        )}
       </form>
       <a href="">Sign up</a>
     </div>
