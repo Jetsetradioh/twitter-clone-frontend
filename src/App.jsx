cea6815a6324f5f84d45d74f355942893955ed99
Max Dahlkvist <max.dahlkvist@hotmail.com>
2025-05-05 13:11:10 +0200

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Signup from './components/signup/signup';
 import Sidebar from './components/sidebar/sidebar';
+import Navbar from './components/navbar/navbar';
 
 function App() {
   return (
     <div className="App">
       <Signup />
       <Sidebar />
+      <Navbar/>
     </div>
   );
 }
