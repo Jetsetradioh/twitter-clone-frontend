5c6f1a0eaed3bf7a785ca1057ec0de3097b2fc40
Max Dahlkvist <max.dahlkvist@hotmail.com>
2025-05-05 12:34:02 +0200

    changed classnames

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -3,36 +3,36 @@ import './sidebar.css';
 
 const Sidebar = () => {
     return (
-        <div className="sidebar">
-            <h3 className="title">Trends for you</h3>
+        <div className="sidebar-sidebar">
+            <h3 className="sidebar-title">Trends for you</h3>
 
-            <div className="trend">
-                <p className="meta">Trending in Sweden <span className="dots">⋯</span></p>
-                <p className="topic">Samt</p>
-                <p className="tweets">2,840 Tweets</p>
+            <div className="sidebar-trend">
+                <p className="sidebar-meta">Trending in Sweden <span className="sidebar-dots">⋯</span></p>
+                <p className="sidebar-topic">Samt</p>
+                <p className="sidebar-tweets">2,840 Tweets</p>
             </div>
 
-            <div className="trend">
-                <p className="meta">Politics · Trending <span className="dots">⋯</span></p>
-                <p className="topic">China</p>
-                <p className="tweets">572K Tweets</p>
+            <div className="sidebar-trend">
+                <p className="sidebar-meta">Politics · Trending <span className="sidebar-dots">⋯</span></p>
+                <p className="sidebar-topic">China</p>
+                <p className="sidebar-tweets">572K Tweets</p>
             </div>
 
-            <div className="trend">
-                <p className="meta">Trending in Sweden <span className="dots">⋯</span></p>
-                <p className="topic">Israel</p>
-                <p className="tweets">10.2K Tweets</p>
+            <div className="sidebar-trend">
+                <p className="sidebar-meta">Trending in Sweden <span className="sidebar-dots">⋯</span></p>
+                <p className="sidebar-topic">Israel</p>
+                <p className="sidebar-tweets">10.2K Tweets</p>
             </div>
 
-            <div className="trend">
-                <p className="meta">Trending in Sweden <span className="dots">⋯</span></p>
-                <p className="topic">#babygirl</p>
+            <div className="sidebar-trend">
+                <p className="sidebar-meta">Trending in Sweden <span className="sidebar-dots">⋯</span></p>
+                <p className="sidebar-topic">#babygirl</p>
             </div>
 
-            <div className="trend">
-                <p className="meta">Trending in Sweden <span className="dots">⋯</span></p>
-                <p className="topic">Newroz</p>
-                <p className="tweets">60.4K Tweets</p>
+            <div className="sidebar-trend">
+                <p className="sidebar-meta">Trending in Sweden <span className="sidebar-dots">⋯</span></p>
+                <p className="sidebar-topic">Newroz</p>
+                <p className="sidebar-tweets">60.4K Tweets</p>
             </div>
         </div>
     );
