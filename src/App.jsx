6c59c20009db7c4fd94c5509448de6db03cc525e
Max Dahlkvist <max.dahlkvist@hotmail.com>
2025-05-05 11:23:06 +0200

    coded html for sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Signup from './components/signup/signup';
 import './App.css';
+import Sidebar from './components/sidebar/sidebar';
 
 function App() {
   return (
     <div className="App">
       <Signup />
+      <Sidebar />
     </div>
   );
 }
