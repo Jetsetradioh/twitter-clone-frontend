d2e4c4f62bd892aa86a43ff963b0311fba929aa2
FilipLiljenberg <filipliljenberg9@.com>
2025-05-16 10:53:04 +0200

    Save

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
 import "./profile.css";
+import Tweet from "../tweet/tweet";
 const Profile = ({ loggedUser }) => {
   const {
+    _id,
     name,
     username,
     bio,
@@ -14,13 +16,18 @@ const Profile = ({ loggedUser }) => {
     followingCount,
   } = loggedUser;
 
+  useEffect(async () => {
+    const response = await fetch(`http://localhost:3000/api/tweets/${_id}`);
+    const data = await response.json();
+  }, []);
+
   return (
     <div className="profile-container">
       <div className="profile-header">
         <i id="profile-left-icon" className="fa-solid fa-arrow-left"></i>{" "}
         <div className="profile-header-box">
           <h2>{name}</h2>
-          <h6>{tweetsCount} tweets</h6>
+          <h6>{tweetsCount} tweets </h6>
         </div>
       </div>
       <img className="profile-banner" src={bannerImage}></img>
